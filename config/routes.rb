f120684a58300c028c44dc9ceeef3968df11db8f
Bill Wu <billwu93@gmail.com>
2022-06-16 05:44:36 +0000

    Added Controllers and 2 pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'simonsays' => 'static_pages#simonsays'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'application#hello'
 end
